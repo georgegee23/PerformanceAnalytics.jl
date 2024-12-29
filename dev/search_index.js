6bc5cfd2b9a13e0d0e0cacf20d01d5033f0b9f85
Documenter.jl <documenter@juliadocs.github.io>
2024-12-29 18:32:48 +0000

    build based on 047015b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PerformanceAnalytics","category":"page"},{"location":"#PerformanceAnalytics","page":"Home","title":"PerformanceAnalytics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PerformanceAnalytics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PerformanceAnalytics]","category":"page"}]
+}
